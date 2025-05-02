edcd37d4cec4c20c17c174555f3ae92aaf4cc3de
raja <marwah@MacBook-Air-de-Marwah.local>
2025-05-02 10:39:44 +0200

    re

---
+++ test.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        sum = 0
+        n = len(nums)
+        for num in nums:
+            sum =
+            
+
+            
+        
